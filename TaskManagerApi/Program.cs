af3618defecf7cc15c89d092b1abe7eeaa81bc90
Sai teja <saitejapala@gmail.com>
2025-12-01 00:17:27 +0530

    for Sqlserver

--- TaskManagerApi/Program.cs
+++ TaskManagerApi/Program.cs
@@ -23,11 +23,12 @@ builder.Services.AddDbContext<TaskManagerApi.Infrastructure.Data.AppDbContext>(o
 {
     if (DbPorovider == "PostgreSQL")
     {
-        connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
-    }
-    else // Default to Sqlserver
-    {
         connectionString = builder.Configuration.GetConnectionString("PgDefaultConnection") ?? string.Empty;
+    }
+    else // Default to SqlServer
+    {
+        connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
+        options.UseSqlServer(connectionString, x => x.MigrationsAssembly("TaskManagerApi.Infrastructure"));
     }
 });
 
